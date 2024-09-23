7e2deecc49d6018a4ec77ff666bf84464acd535a
Evan <evanaditya18@gmail.com>
2024-09-24 01:05:13 +0700

    finishing home layout

--- app/src/main/java/com/example/jlaja/MainActivity.kt
+++ app/src/main/java/com/example/jlaja/MainActivity.kt
@@ -16,17 +16,7 @@ import com.example.jlaja.ui.theme.JlajaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            JlajaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.home)
     }
 }
 
