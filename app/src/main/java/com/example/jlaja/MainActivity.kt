9d83c376801f720216556ee369d835b6c32e59a8
Evan <evanaditya18@gmail.com>
2024-10-06 12:13:36 +0700

    Renaming & remapping constraint

--- app/src/main/java/com/example/jlaja/MainActivity.kt
+++ app/src/main/java/com/example/jlaja/MainActivity.kt
@@ -16,7 +16,7 @@ import com.example.jlaja.ui.theme.JlajaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+        setContentView(R.layout.login)
     }
 }
 
