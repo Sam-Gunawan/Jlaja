d683c43d683233126aa0e4bb7387534c6790c7d2
Rainer <rainer.dylan2308@gmail.com>
2024-11-17 19:27:47 +0700

    split bill main page

--- app/src/main/java/com/example/uiux/SplitBillPage.kt
+++ app/src/main/java/com/example/uiux/SplitBillPage.kt
@@ -1,6 +1,8 @@
 package com.example.uiux
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,12 @@ class SplitBillPage : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun toSecondTimeUserHome(view: View?) {
+        val intent = Intent(
+            this@SplitBillPage,
+            SecondTimeUserHome::class.java
+        )
+        startActivity(intent)
     }
 }
