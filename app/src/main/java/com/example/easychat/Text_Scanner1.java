cd1ea42ecf5971cfc82c8f2858fd38d6b54dcc48
Samuel <samistheguns@gmail.com>
2024-11-18 16:25:59 +0700

    Fixed deprecated versions

--- app/src/main/java/com/example/easychat/Text_Scanner1.java
+++ app/src/main/java/com/example/easychat/Text_Scanner1.java
@@ -1,70 +1,49 @@
-package com.example.easychat;
-
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-//import com.theartofdev.edmodo.cropper.CropImage;
-//import com.theartofdev.edmodo.cropper.CropImageView;
-
-public class Text_Scanner1 extends AppCompatActivity {
-    Button button_capture, button_copy;
-    TextView textView_data;
-    Bitmap bitmap;
-    private static final int REQUEST_CAMERA_CODE = 100;
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_text_scanner1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        button_capture = findViewById(R.id.button_capture);
-        button_copy = findViewById(R.id.button_copy);
-        textView_data = findViewById(R.id.text_data);
-
-        if(ContextCompat.checkSelfPermission(Text_Scanner1.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(Text_Scanner1.this, new String[]{
-                    android.Manifest.permission.CAMERA
-            }, REQUEST_CAMERA_CODE);
-        }
-
-        //UNTUK CROPPING 1
-        button_capture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                start(Text_Scanner1.this);
-            }
-        });
-    }
-
-    //UNTUK CROPPING 2
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == )
-    }
-
-    private void
-}
+//package com.example.easychat;
+//
+//import android.content.pm.PackageManager;
+//import android.graphics.Bitmap;
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import androidx.activity.EdgeToEdge;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.app.ActivityCompat;
+//import androidx.core.content.ContextCompat;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
+//
+////import com.theartofdev.edmodo.cropper.CropImage;
+////import com.theartofdev.edmodo.cropper.CropImageView;
+//
+//public class Text_Scanner1 extends AppCompatActivity {
+//    Button button_capture, button_copy;
+//    TextView textView_data;
+//    Bitmap bitmap;
+//    private static final int REQUEST_CAMERA_CODE = 100;
+//
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_text_scanner1);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//
+//        button_capture = findViewById(R.id.button_capture);
+//        button_copy = findViewById(R.id.button_copy);
+//        textView_data = findViewById(R.id.text_data);
+//
+//        if (ContextCompat.checkSelfPermission(Text_Scanner1.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(Text_Scanner1.this, new String[]{
+//                    android.Manifest.permission.CAMERA
+//            }, REQUEST_CAMERA_CODE);
+//        }
+//    }
+//}
