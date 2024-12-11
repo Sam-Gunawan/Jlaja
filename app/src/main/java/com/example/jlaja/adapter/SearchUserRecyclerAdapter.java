dc8694a8f4acc5be79273668ebf24ddfaf78b51a
Evan <evanaditya18@gmail.com>
2024-12-11 17:41:19 +0700

    adding chat and profile

---
+++ app/src/main/java/com/example/jlaja/adapter/SearchUserRecyclerAdapter.java
@@ -0,0 +1,74 @@
+package com.example.jlaja.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.jlaja.ChatActivity;
+import com.example.jlaja.R;
+import com.example.jlaja.model.UserModel;
+import com.example.jlaja.utils.AndroidUtil;
+import com.example.jlaja.utils.FirebaseUtil;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class SearchUserRecyclerAdapter extends FirestoreRecyclerAdapter<UserModel, SearchUserRecyclerAdapter.UserModelViewHolder> {
+
+    Context context;
+
+    public SearchUserRecyclerAdapter(@NonNull FirestoreRecyclerOptions<UserModel> options,Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull UserModelViewHolder holder, int position, @NonNull UserModel model) {
+        holder.usernameText.setText(model.getUsername());
+        if(model.getUserId().equals(FirebaseUtil.currentUserId())){
+            holder.usernameText.setText(model.getUsername()+" (Me)");
+        }
+
+        FirebaseUtil.getOtherProfilePicStorageRef(model.getUserId()).getDownloadUrl()
+                .addOnCompleteListener(t -> {
+                    if(t.isSuccessful()){
+                        Uri uri  = t.getResult();
+                        AndroidUtil.setProfilePic(context,uri,holder.profilePic);
+                    }
+                });
+
+        holder.itemView.setOnClickListener(v -> {
+            //navigate to chat activity
+            Intent intent = new Intent(context, ChatActivity.class);
+            AndroidUtil.passUserModelAsIntent(intent,model);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        });
+    }
+
+    @NonNull
+    @Override
+    public UserModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.search_user_recycler_row,parent,false);
+        return new UserModelViewHolder(view);
+    }
+
+    class UserModelViewHolder extends RecyclerView.ViewHolder{
+        TextView usernameText;
+
+        ImageView profilePic;
+
+        public UserModelViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usernameText = itemView.findViewById(R.id.user_name_text);
+            profilePic = itemView.findViewById(R.id.profile_pic_image_view);
+        }
+    }
+}
